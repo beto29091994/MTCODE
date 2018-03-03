7800cf44f0465e46f3d0cd4bde6c547c995b53c0
beto2909 <albertojr94@hotmail.com>
2018-03-02 21:07:54 -0800

    MT finished

---
+++ ContosoUniversity/Models/Person.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public abstract class Person
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+        public Address Email { get; set; }
+
+        public class Address
+        {
+            [Display(Name = "Email")]
+            [RegularExpression("^[A-Za-z0-9._%+-]+@contoso.edu$", ErrorMessage = "Invalid Email Id")]
+            public string Email { get; set; }
+
+            public Address(string e)
+            {
+                Email = e;
+            }
+
+            public Address()
+            {
+                Email = "unknown@contoso.edu";
+            }
+
+        }
+
+        
+    }
+}
